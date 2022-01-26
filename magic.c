04178fdfea2238fbfc58fdbaae59cbad00a50689
JordanFosu <91875405+JordanFosu@users.noreply.github.com>
2022-01-26 21:19:16 +0000

    Create magic.c

---
+++ magic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#includs <time.h>
+
+int main(int argc,char *argv[]){
+
+printf("Welcome to github");
+
+return 0;
+
+
+}
